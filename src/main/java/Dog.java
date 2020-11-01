f1d926f7dcd377b72c0ac996f1cec38555835a61
22rmisri <22rmisri@athenian.org>
2020-11-01 18:45:35 +0000

    Added code to Dog.java and tested in the DogClient.java

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,41 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
 
+       // Constructors
 
-    // Constructors
+    public Dog(String a, int b, String c) {
+        super(a, b);
+        this.breed = c;
+    }
+
+    public Dog() {
+        super("Max", 1);
+        this.breed = "labrador";
+    }
     
 
-    // makeNoise() method
+    // makeNoise() 
+    public void makeNoise() {
+        System.out.println("Ruff");
+    }
 
 
     // toString method
-
+    public String toString() {
+        String str = "Name:" + this.getName() + " Age:" + this.getAge() + " Breed:" + this.breed;
+        return str;
+    }
 
     // Getter
-
-
+    public String getBreed() {
+        return this.breed;
+    }
     // Setter
+    public void setBreed() {
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
+    
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,11 @@
 public class DogClient {
     public static void main(String[] args) {
+        Dog d1 = new Dog("doggy", 5, "golden retriever");
+        System.out.println(d1);
+        d1.makeNoise();
+        Dog d2 = new Dog();
+        System.out.println(d2);
+
         
     }
 }
